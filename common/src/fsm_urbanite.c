7dac3093e330ba2852f75b05bcb5f372827293f1
JorgeLGRDA <jorge.lopezgalvez.ramirez@gmail.com>
2025-04-23 18:09:42 +0200

    V4 commented?

--- common/src/fsm_urbanite.c
+++ common/src/fsm_urbanite.c
@@ -1,19 +1,42 @@
+/**
+ * @file fsm_urbanite.c
+ * @brief Implementation of the finite state machine (FSM) for the Urbanite system.
+ * @author Marcos Perez
+ * @author Jorge Lopez-Galvez
+ * * @date fecha
+ */
+
+/* Includes ------------------------------------------------------------------*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "port_system.h"
 #include "fsm.h"
 #include "fsm_urbanite.h"
 
-
+/* Typedefs ------------------------------------------------------------------*/
+/**
+ * @brief Structure representing the FSM for the Urbanite system.
+ */
 struct fsm_urbanite_t{
-    fsm_t f;
-    fsm_button_t * p_fsm_button;
-    uint32_t on_off_press_time_ms;
-    uint32_t pause_display_time_ms;
-    bool is_paused;
-    fsm_ultrasound_t * p_fsm_ultrasound_rear;
-    fsm_display_t * p_fsm_display_rear;
+    fsm_t f; /*!< Base FSM structure */
+    fsm_button_t * p_fsm_button; /*!< Pointer to the button FSM. */
+    uint32_t on_off_press_time_ms; /*!< Time in milliseconds to toggle the system on/off. */
+    uint32_t pause_display_time_ms; /*!< Time in milliseconds to pause/resume the display. */
+    bool is_paused; /*!< Indicates if the display is paused. */
+    fsm_ultrasound_t * p_fsm_ultrasound_rear; /*!< Pointer to the rear ultrasound FSM. */
+    fsm_display_t * p_fsm_display_rear; /*!< Pointer to the rear display FSM.  */
 };
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+ * @brief Checks if the system should be turned on.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if the system should be turned on, `false` otherwise.
+ */
 
 static bool check_on (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
@@ -23,14 +46,38 @@ static bool check_on (fsm_t *p_this){
     return false;
 }
 
+/**
+ * @brief Checks if the system should be turned off.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if the system should be turned off, `false` otherwise.
+ */
+
 static bool check_off (fsm_t *p_this){
    return check_on(p_this);
 }
+
+/**
+ * @brief Checks if a new ultrasound measurement is ready.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if a new measurement is ready, `false` otherwise.
+ */
 
 static bool check_new_measure (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
     return fsm_ultrasound_get_new_measurement_ready(p_fsm_urbanite->p_fsm_ultrasound_rear);
 }
+
+/**
+ * @brief Checks if the display should be paused or resumed.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if the display should be paused/resumed, `false` otherwise.
+ */
 
 static bool check_pause_display (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
@@ -40,18 +87,48 @@ static bool check_pause_display (fsm_t *p_this){
     return false;
 }
 
+/**
+ * @brief Checks if there is activity in any of the subsystems.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if there is activity, `false` otherwise.
+ */
+
 static bool check_activity (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
     return (fsm_button_check_activity(p_fsm_urbanite->p_fsm_button) || fsm_ultrasound_check_activity(p_fsm_urbanite->p_fsm_ultrasound_rear) || fsm_display_check_activity(p_fsm_urbanite->p_fsm_display_rear));
 }
 
+/**
+ * @brief Checks if there is no activity in any of the subsystems.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if there is no activity, `false` otherwise.
+ */
+
 static bool check_no_activity (fsm_t *p_this){
     return (!check_activity(p_this));
 }
 
+/**
+ * @brief Checks if there is activity in the measurement state.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ * 
+ * @return `true` if there is activity in the measurement state, `false` otherwise.
+ */
+
 static bool check_activity_in_measure (fsm_t *p_this){
     return check_new_measure(p_this); 
 }
+
+/**
+ * @brief Starts the Urbanite system and initializes measurements.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
 
 static void do_start_up_measure (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
@@ -60,6 +137,12 @@ static void do_start_up_measure (fsm_t *p_this){
     fsm_display_set_status(p_fsm_urbanite->p_fsm_display_rear, true);
     printf("[URBANITE][%ld] Urbanite system ON\n", port_system_get_millis());
 }
+
+/**
+ * @brief Displays the distance measured by the ultrasound sensor.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
 
 static void do_display_distance (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
@@ -80,6 +163,12 @@ static void do_display_distance (fsm_t *p_this){
     }
 }
 
+/**
+ * @brief Pauses or resumes the display.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
+
 static void do_pause_display (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
 
@@ -96,6 +185,12 @@ static void do_pause_display (fsm_t *p_this){
     }
 }
  
+/**
+ * @brief Stops the Urbanite system.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
+
 static void do_stop_urbanite (fsm_t *p_this){
     fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *) p_this;
 
@@ -112,13 +207,31 @@ static void do_stop_urbanite (fsm_t *p_this){
     printf("[URBANITE][%ld] Urbanite system OFF\n", port_system_get_millis());
 }
 
+/**
+ * @brief Puts the system to sleep while off.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
+
 static void do_sleep_while_off (fsm_t *p_this){
     port_system_sleep();
 }
 
+/**
+ * @brief Puts the system to sleep while on.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
+
 static void do_sleep_while_on (fsm_t *p_this){
     port_system_sleep();
 }
+
+/**
+ * @brief Puts the system to sleep while in the measurement state.
+ * 
+ * @param p_this Pointer to the FSM instance.
+ */
 
 static void do_sleep_while_measure (fsm_t *p_this){
     port_system_sleep();
@@ -127,6 +240,10 @@ static void do_sleep_while_measure (fsm_t *p_this){
 static void do_sleep_off (fsm_t *p_this){
     port_system_sleep();
 }
+
+/**
+ * @brief Transition table for the Urbanite FSM.
+ */
 
 static fsm_trans_t fsm_trans_urbanite[] = {
     {OFF, check_no_activity, SLEEP_WHILE_OFF, do_sleep_off},
@@ -142,6 +259,18 @@ static fsm_trans_t fsm_trans_urbanite[] = {
     {-1, NULL, -1, NULL},
 };
 
+/* Public functions ----------------------------------------------------------*/
+
+/**
+ * @brief Initializes the Urbanite FSM.
+ * 
+ * @param p_fsm_urbanite Pointer to the Urbanite FSM instance.
+ * @param p_fsm_button Pointer to the button FSM.
+ * @param on_off_press_time_ms Time in milliseconds to toggle the system on/off.
+ * @param pause_display_time_ms Time in milliseconds to pause/resume the display.
+ * @param p_fsm_ultrasound_rear Pointer to the rear ultrasound FSM.
+ * @param p_fsm_display_rear Pointer to the rear display FSM.
+ */
 
 static void fsm_urbanite_init (fsm_urbanite_t *p_fsm_urbanite, fsm_button_t *p_fsm_button, uint32_t on_off_press_time_ms, uint32_t pause_display_time_ms, fsm_ultrasound_t *p_fsm_ultrasound_rear, fsm_display_t *p_fsm_display_rear){
         
@@ -156,6 +285,18 @@ static void fsm_urbanite_init (fsm_urbanite_t *p_fsm_urbanite, fsm_button_t *p_f
     p_fsm_urbanite->is_paused = false;
 }
 
+/**
+ * @brief Creates a new Urbanite FSM instance.
+ * 
+ * @param p_fsm_button Pointer to the button FSM.
+ * @param on_off_press_time_ms Time in milliseconds to toggle the system on/off.
+ * @param pause_display_time_ms Time in milliseconds to pause/resume the display.
+ * @param p_fsm_ultrasound_rear Pointer to the rear ultrasound FSM.
+ * @param p_fsm_display_rear Pointer to the rear display FSM.
+ *
+ * @return Pointer to the newly created Urbanite FSM instance.
+ */
+
 fsm_urbanite_t *fsm_urbanite_new (fsm_button_t *p_fsm_button, uint32_t on_off_press_time_ms, uint32_t pause_display_time_ms, fsm_ultrasound_t *p_fsm_ultrasound_rear, fsm_display_t *p_fsm_display_rear){
 
         fsm_urbanite_t *p_fsm_urbanite = (fsm_urbanite_t *)malloc(sizeof(fsm_urbanite_t));
@@ -164,10 +305,23 @@ fsm_urbanite_t *fsm_urbanite_new (fsm_button_t *p_fsm_button, uint32_t on_off_pr
     
         return p_fsm_urbanite;
 }
+
+/**
+ * @brief Triggers the Urbanite FSM to process its current state and transition if necessary.
+ * 
+ * @param p_fsm_urbanite Pointer to the Urbanite FSM instance.
+ */
+
 void fsm_urbanite_fire (fsm_urbanite_t *p_fsm_urbanite){
     fsm_fire(&p_fsm_urbanite->f);
 }
  
+**
+ * @brief Destroys the Urbanite FSM instance and frees its resources.
+ * 
+ * @param p_fsm Pointer to the Urbanite FSM instance to destroy.
+ */
+
 void fsm_urbanite_destroy (fsm_urbanite_t *p_fsm){
     free(&p_fsm->f);
 }
